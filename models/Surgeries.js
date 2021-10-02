09e2edec866c1db24443d00ccd11dcad21331efd
Mateus Alencar <mtauws@gmail.com>
2021-10-02 20:54:11 -0300

    ADD MODEL PatientSurgiries and Surgery

---
+++ models/Surgeries.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const Surgery = sequelize.define(
+    'Surgery',
+    {
+      surgery_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      specialty: DataTypes.STRING,
+      doctor: DataTypes.STRING,
+    },
+    { timestamps: false, tableName: 'Surgerys', underscored: true }
+  );
+  return Surgery;
+};
