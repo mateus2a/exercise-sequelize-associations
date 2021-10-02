e802362e498d5cc8a3866822e807e14fb19ec9cc
Mateus Alencar <mtauws@gmail.com>
2021-10-02 20:48:05 -0300

    creation of models

---
+++ models/Patients.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const Patient = sequelize.define(
+    'Patient',
+    {
+      patient_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      fullname: DataTypes.STRING,
+      plan_id: { type: DataTypes.INTEGER, foreignKey: true },
+    },
+    { timestamps: false, tableName: 'Patients', underscored: true }
+  );
+  return Patient;
+};
